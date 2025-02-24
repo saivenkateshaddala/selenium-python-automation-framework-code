5d3e95c67a8d8fdb06634392d8e04a5a4976f83e
sai venkatesh <addalasaicse2019@gmail.com>
2025-02-24 12:19:06 +0530

    sdet1 commit to feature abc

--- Testcases/test_sdet2.py
+++ Testcases/test_sdet2.py
@@ -2,3 +2,10 @@
 #Add some lines in the sde2
 #Add few more lines in sdet3
 #Add one more in sdet2
+#multiple  line added line 1
+#multiple  line added line 2
+#multiple  line added line 3
+#multiple  line added line 4
+#multiple  line added line 5
+#multiple  line added line 6
+#multiple  line added line 6
